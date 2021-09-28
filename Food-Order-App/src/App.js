fdc1f02f57605d35827e8737d05e04c6314b4e66
KORAH <korahabhishek@gmail.com>
2021-09-28 18:29:56 +0530

    Reactjs Food-Order Project

--- Food-Order-App/src/App.js
+++ Food-Order-App/src/App.js
@@ -1,8 +1,31 @@
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./components/Store/CartProvider";
+
 function App() {
+
+  const [cartIsShown, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  }
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+      // <Fragment>
+      <CartProvider>
+        {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+        <Header onShowCart={showCartHandler}/>
+        <main>
+          <Meals />
+        </main>
+      </CartProvider>
+      // </Fragment>
   );
 }
 
